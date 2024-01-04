7ce76ec1569a15bb3989103de04a40fc47b3293f
epyon <epyon@chaosforge.org>
2024-01-04 15:17:50 +0100

    QuadList and QuadRenderer to sub for old code

--- src/vglquadbuffer.pas
+++ src/vglquadbuffer.pas
@@ -2,7 +2,7 @@
 unit vglquadbuffer;
 interface
 
-uses Classes, SysUtils, vgltypes, vgenerics, vvector, vglprogram, vgldrawarrays,
+uses Classes, SysUtils, vnode, vgltypes, vglprogram, vgldrawarrays,
      vglquadarrays;
 
 type TGLTexturedColoredQuadLayer = class( TGLTexturedArrays )
@@ -11,9 +11,82 @@ public
   property Quads[ aTextureID : Cardinal ]  : TGLTexturedColoredQuads read GetArray; default;
 end;
 
+
+type TGLQuadList = class
+  //procedure PostColoredLine( aFrom, aTo : TGLVec2i; aColor : TGLVec4f );
+  //procedure PostColoredQuad( aUR, aLL : TGLVec2i; aColor : TGLVec4f );
+  //procedure PostTexturedQuad( aUR, aLL : TGLVec2i; aTUR, aTLL : TGLVec2f; aTexture : DWord );
+  //procedure PostTexturedQuad( aUR, aLL : TGLVec2i; aColor : TGLVec4f; aTUR, aTLL : TGLVec2f; aTexture : DWord );
+  //procedure PostTexturedQuad( aUR, aLL : TGLVec2i; aColor : TGLRawQColor4f; aTUR, aTLL : TGLVec2f; aTexture : DWord );
+  procedure Append( aList : TGLTexturedColoredQuads; aTexture : DWord );
+  destructor Destroy; override;
+private
+  FColored   : TGLColoredQuads;
+  FTextured  : TGLTexturedColoredQuadLayer;
+end;
+
+type TGLQuadRenderer = class( TVObject )
+  constructor Create;
+  procedure Update( aProjection : TMatrix44 );
+  procedure Render( aList : TGLQuadList );
+  destructor Destroy; override;
+private
+  FProjection  : TMatrix44;
+  FCProgram    : TGLProgram;
+  FTProgram    : TGLProgram;
+end;
+
 implementation
 
 uses vgl3library;
+
+const
+GLQuadColorVertexShader : Ansistring =
+'#version 330 core'+#10+
+'layout (location = 0) in vec3 position;'+#10+
+'layout (location = 2) in vec4 color;'+#10+
+'uniform mat4 utransform;'+#10+
+#10+
+'out vec4 ocolor;'+#10+
+#10+
+'void main() {'+#10+
+'ocolor = color;'+#10+
+'gl_Position = utransform * vec4(position, 1.0);'+#10+
+'}'+#10;
+GLQuadColorFragmentShader : Ansistring =
+'#version 330 core'+#10+
+'in vec4 ocolor;'+#10+
+'out vec4 frag_color;'+#10+
+#10+
+'void main() {'+#10+
+'frag_color = ocolor;'+#10+
+'}'+#10;
+GLQuadTextureVertexShader : Ansistring =
+'#version 330 core'+#10+
+'layout (location = 0) in vec3 position;'+#10+
+'layout (location = 1) in vec2 texcoord;'+#10+
+'layout (location = 2) in vec4 color;'+#10+
+'uniform mat4 utransform;'+#10+
+#10+
+'out vec4 ocolor;'+#10+
+'out vec2 otexcoord;'+#10+
+#10+
+'void main() {'+#10+
+'otexcoord = texcoord;'+#10+
+'ocolor = color;'+#10+
+'gl_Position = utransform * vec4(position, 1.0);'+#10+
+'}'+#10;
+GLQuadTextureFragmentShader : Ansistring =
+'#version 330 core'+#10+
+'in vec4 ocolor;'+#10+
+'in vec2 otexcoord;'+#10+
+'out vec4 frag_color;'+#10+
+#10+
+'uniform sampler2D utexture;'+#10+
+#10+
+'void main() {'+#10+
+'frag_color = ocolor * texture( utexture, otexcoord );'+#10+
+'}'+#10;
 
 { TGLTexturedColoredQuadLayer }
 
@@ -28,6 +101,75 @@ begin
   end;
 end;
 
+
+procedure TGLQuadList.Append ( aList : TGLTexturedColoredQuads; aTexture : DWord ) ;
+var iIndex : Integer;
+begin
+  if not Assigned( FTextured ) then FTextured := TGLTexturedColoredQuadLayer.Create;
+  FTextured[aTexture].Append( aList );
+end;
+
+destructor TGLQuadList.Destroy;
+begin
+  FreeAndNil( FColored );
+  FreeAndNil( FTextured );
+  inherited Destroy;
+end;
+
+constructor TGLQuadRenderer.Create;
+begin
+  inherited Create;
+  FillChar( FProjection, SizeOf( FProjection ), 0 );
+  FCProgram   := TGLProgram.Create( GLQuadColorVertexShader,   GLQuadColorFragmentShader );
+  FTProgram   := TGLProgram.Create( GLQuadTextureVertexShader, GLQuadTextureFragmentShader );
+end;
+
+procedure TGLQuadRenderer.Update ( aProjection : TMatrix44 );
+var i : Integer;
+begin
+  for i := 0 to 15 do
+    if FProjection[i] <> aProjection[i] then
+    begin
+      FProjection := aProjection;
+      FCProgram.Bind;
+      glUniformMatrix4fv( FCProgram.GetUniformLocation( 'utransform' ), 1, GL_FALSE, @FProjection[0] );
+      FCProgram.UnBind;
+      FTProgram.Bind;
+      glUniformMatrix4fv( FTProgram.GetUniformLocation( 'utransform' ), 1, GL_FALSE, @FProjection[0] );
+      glUniform1i( FTProgram.GetUniformLocation('utexture'), 0 );
+      FTProgram.UnBind;
+      Exit;
+    end;
+end;
+
+procedure TGLQuadRenderer.Render ( aList : TGLQuadList );
+begin
+  if (aList.FTextured <> nil) and (not aList.FTextured.Empty) then
+  begin
+    aList.FTextured.Update;
+    FTProgram.Bind;
+    glActiveTexture(GL_TEXTURE0);
+    aList.FTextured.Draw;
+    FTProgram.UnBind;
+    aList.FTextured.Clear;
+  end;
+
+  if (aList.FColored <> nil) and (not aList.FColored.Empty) then
+  begin
+    aList.FColored.Update;
+    FCProgram.Bind;
+    aList.FColored.Draw;
+    FTProgram.UnBind;
+    aList.FColored.Clear;
+  end;
+end;
+
+destructor TGLQuadRenderer.Destroy;
+begin
+  inherited Destroy;
+  FreeAndNil( FCProgram );
+  FreeAndNil( FTProgram );
+end;
 
 
 end.
