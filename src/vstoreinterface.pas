e209d590607c88f1570243e1633f2552dffa32b4
epyon <epyon@chaosforge.org>
2025-06-30 20:58:34 +0200

    Steam support for Valkyrie

---
+++ src/vstoreinterface.pas
@@ -0,0 +1,158 @@
+{$include valkyrie.inc}
+unit vstoreinterface;
+interface
+uses vnode, vgenerics;
+
+type TStoreType = ( StoreNone, StoreSteam );
+
+type TModInfo = class
+  private
+    FFolder : Ansistring;
+    FID     : QWord;
+  public
+    constructor Create( aFolder : Ansistring; aID : QWord );
+  public
+    property Folder : Ansistring read FFolder;
+    property ID     : QWord      read FID;
+  end;
+
+type TModArray = specialize TGObjectArray< TModInfo >;
+
+type TStoreInterface = class( TVObject )
+  class function Get : TStoreInterface;
+  function GetUserId : QWord; virtual;
+  function IsInitialized : Boolean; virtual;
+  function IsOverlayEnabled : Boolean; virtual;
+  function GetUsername : Ansistring; virtual;
+  procedure Update; virtual;
+  function SetAchievement( const aID : Ansistring ) : Boolean; virtual;
+  function IncStat( const aID : Ansistring ) : Boolean; virtual;
+  function GetGlobalStat( const aID : Ansistring ) : Int64; virtual;
+  function FlushStatistics : Boolean; virtual;
+  function OpenDLCPage( aAppID : DWord ) : Boolean; virtual;
+  function OpenStorePage( aAppID : DWord ) : Boolean; virtual;
+  function GetStoreType : TStoreType; virtual;
+  function ModPublish( const aPath, aModID : Ansistring ) : QWord; virtual;
+  function ModUpdate( const aPath : Ansistring; aModID : QWord ) : Boolean; virtual;
+  function GetMods : TModArray; virtual;
+  destructor Destroy; override;
+protected
+  constructor Create;
+end;
+
+
+implementation
+
+uses sysutils, vsteam, vdebug;
+
+var GStoreInterface : TStoreInterface = nil;
+
+constructor TModInfo.Create( aFolder : Ansistring; aID : QWord );
+begin
+  FFolder := aFolder;
+  FID     := aId;
+end;
+
+class function TStoreInterface.Get : TStoreInterface;
+begin
+  if Assigned ( GStoreInterface ) then Exit( GStoreInterface );
+  if TSteam.TryLoadLibrary then
+    GStoreInterface := TSteam.Create
+  else
+  begin
+    vdebug.Log('TStoreInterface : no store API detected, initializing stub');
+    GStoreInterface := TStoreInterface.Create;
+  end;
+  Exit( GStoreInterface );
+end;
+
+function TStoreInterface.GetUserId : QWord;
+begin
+  Exit( 0 );
+end;
+
+function TStoreInterface.IsInitialized : Boolean;
+begin
+  Exit( False );
+end;
+
+function TStoreInterface.IsOverlayEnabled : Boolean;
+begin
+  Exit( False );
+end;
+
+function TStoreInterface.GetUsername : Ansistring;
+begin
+  Exit( '' );
+end;
+
+procedure TStoreInterface.Update;
+begin
+end;
+
+function TStoreInterface.SetAchievement( const aID : Ansistring ) : Boolean;
+begin
+  Exit( False );
+end;
+
+function TStoreInterface.IncStat( const aID : Ansistring ) : Boolean;
+begin
+  Exit( False );
+end;
+
+function TStoreInterface.GetGlobalStat( const aID : Ansistring ) : Int64;
+begin
+  Exit( 0 );
+end;
+
+function TStoreInterface.FlushStatistics : Boolean;
+begin
+  Exit( False );
+end;
+
+function TStoreInterface.OpenDLCPage( aAppID : DWord ) : Boolean;
+begin
+  Exit( False );
+end;
+
+function TStoreInterface.OpenStorePage( aAppID : DWord ) : Boolean;
+begin
+  Exit( False );
+end;
+
+function TStoreInterface.GetStoreType : TStoreType;
+begin
+  Exit( StoreNone );
+end;
+
+function TStoreInterface.ModPublish( const aPath, aModID : Ansistring ) : QWord;
+begin
+  Exit( 0 );
+end;
+
+function TStoreInterface.ModUpdate( const aPath : Ansistring; aModID : QWord ) : Boolean;
+begin
+  Exit( False );
+end;
+
+function TStoreInterface.GetMods : TModArray;
+begin
+  Exit( nil );
+end;
+
+destructor TStoreInterface.Destroy;
+begin
+  inherited Destroy;
+end;
+
+constructor TStoreInterface.Create;
+begin
+  inherited Create;
+end;
+
+finalization
+
+FreeAndNil( GStoreInterface );
+
+end.
+
